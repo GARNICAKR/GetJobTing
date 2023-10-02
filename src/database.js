bf554e488d4c717e95e4d3a0b20bf00d1ed67d48
GARNICAKR <jisaias.garnica@alumnos.udg.mx>
2023-10-01 22:40:32 -0600

    Configuracion Deploy DB2

--- src/database.js
+++ src/database.js
@@ -1,5 +1,5 @@
 const mongose =require('mongoose');
-// require('dotenv').config();
+require('dotenv').config();
 //mongose.connect('mongodb://localhost/GetJobTing')
 mongose.connect(process.env.MONGODB_URL)
     .then(db=> console.log('DB is conected'))  
